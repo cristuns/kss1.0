9dfd3b0459125d1a79cf52982d6d0bf4822fa565
cristuns <66400372+cristuns@users.noreply.github.com>
2020-06-06 15:51:47 +0900

    Add files via upload

---
+++ all.js
@@ -0,0 +1,22 @@
+var t=new Date();
+var m=t.getMonth()+1;
+var td =t.getDate();
+var a=td+m;
+var aa=22;
+if(aa==a) {if(m==7){while(true) {alert("끝남"); }} }
+if(aa<a) {if(m==7) {while(true) {alert("끝남");}} }
+if(aa>a) {if(m==8) {while(true) {alert("끝남");}} }
+if(aa==a) {if(m==8) {while(true) {alert("끝남");}} }
+if(aa<a) {if(m==8) {while(true) {alert("끝남");}} }
+if(aa<a) {if(m==9) {while(true) {alert("끝남");}} }
+if(aa==a) {if(m==9) {while(true) {alert("끝남");}} }
+if(aa>a) {if(m==9) {while(true) {alert("끝남");}} }
+if(aa>a) {if(m==10) {while(true) {alert("끝남");}} }
+if(aa==a) {if(m==10) {while(true) {alert("끝남");}} }
+if(aa<a) {if(m==10) {while(true) {alert("끝남");}} }
+if(aa>a) {if(m==11) {while(true) {alert("끝남");}} }
+if(aa==a) {if(m==11) {while(true) {alert("끝남");}} }
+if(aa<a) {if(m==11) {while(true) {alert("끝남");}} }
+if(aa>a) {if(m==12) {while(true) {alert("끝남");}} }
+if(aa==a) {if(m==12) {while(true) {alert("끝남");}} }
+if(aa<a) {if(m==12) {while(true) {alert("끝남");}} }
